6bbeb24dad3a07c8044d88abb1a68bb3bd5507fe
DovilePatiejunaite <dpatiejunaite@gmail.com>
2018-05-27 00:00:14 +0300

    Galutiniai pakeitimai.

---
+++ src/AmortizationSchedule.java
@@ -0,0 +1,127 @@
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class AmortizationSchedule {
+    private Date date;
+    private double interestRate;
+    private BigDecimal loanSize;
+    private int numberOfIncomes;
+    private int paymentNumber = 1;
+    private int months;
+
+
+    AmortizationSchedule(){
+
+    }
+
+    AmortizationSchedule(Date date, double interestRate, BigDecimal loanSize, int numberOfIncomes, int months){
+        this.date = date;
+        this.interestRate = interestRate;
+        this.loanSize = loanSize;
+        this.numberOfIncomes = numberOfIncomes;
+        this.months = months;
+    }
+
+    //Setters and getters
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public void setNumberOfIncomes(int numberOfIncomes) {
+        this.numberOfIncomes = numberOfIncomes;
+    }
+
+    public void setLoanSize(BigDecimal loanSize) {
+        this.loanSize = loanSize;
+    }
+
+    public void setPaymentNumber(int paymentNumber) {
+        this.paymentNumber = paymentNumber;
+    }
+
+    public void setMonths(int months) {
+        this.months = months;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public int getNumberOfIncomes() {
+        return numberOfIncomes;
+    }
+
+    public BigDecimal getLoanSize() {
+        return loanSize;
+    }
+
+    public int getPaymentNumber() {
+        return paymentNumber;
+    }
+
+    public int getMonths() {
+        return months;
+    }
+
+
+    public void generateSchedule(PrintWriter printWriter){
+        //Calculating monthly interest rate from annual interest rate
+        double monthlyRate = getInterestRate()/100/12;
+
+        //Calculating total payment for loan per month
+        double totalPayment = monthlyRate/(1-Math.pow(1+monthlyRate, -getNumberOfIncomes()))*getLoanSize().doubleValue();
+        BigDecimal monthlyPayment = new BigDecimal(totalPayment);
+
+        //Rounding monthly payment - two decimals after comma
+        monthlyPayment = monthlyPayment.setScale(2, RoundingMode.HALF_UP);
+
+        //Generating amortization schedule
+        //Months - how many payments of loan we want to see on schedule (only first ten, all, etc.)
+        //If number of payments to show is set to 0, showing all
+
+        if(getMonths() == 0){
+             setMonths(getNumberOfIncomes());
+        }
+        for(int i = 1; i<= getMonths(); i++){
+
+            //Keeping correct format for date
+            String date = new SimpleDateFormat("MM/dd/yyyy").format(getDate());
+
+            //Monthly interest payment = principal balance (principal loan left to pay) x monthly interest rate
+            BigDecimal interestPayment = new BigDecimal(monthlyRate * getLoanSize().doubleValue());
+            interestPayment = interestPayment.setScale(2, RoundingMode.HALF_UP); // Rounding half up with two decimals after comma
+
+            //Monthly principal payment = monthly payment - monthly interest payment
+            BigDecimal principalPayment = monthlyPayment.subtract(interestPayment);
+            principalPayment = principalPayment.setScale(2, RoundingMode.HALF_UP); // Rounding half up with two decimals after comma
+
+            //Writing to csv file
+            printWriter.print("\""+getPaymentNumber()+"\";\""+ date +"\";\""+getLoanSize()+"\";\""+principalPayment
+                    +"\";\""+interestPayment+"\";\""+monthlyPayment+"\";\""+getInterestRate()+"\"\n");
+
+            // Decreasing principal loan
+            setLoanSize(getLoanSize().subtract(principalPayment));
+
+            //Decreasing date by one month
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(getDate());
+            cal.add(Calendar.MONTH, 1);
+            setDate(cal.getTime());
+
+            //Decreasing payment number
+            setPaymentNumber(getPaymentNumber()+1);
+        }
+    }
+}
---
+++ src/TUI.java
@@ -0,0 +1,151 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+// Class of text user interface
+public class TUI {
+
+    private Scanner input = new Scanner(System.in);
+    //Starting menu
+    public void start(){
+        showMenu();
+        System.out.print("\nĮveskite meniu pasirinkimą: ");
+        int choice = input.nextInt();
+        input.nextLine();
+        while(true){
+            switch(choice){
+                case 0: exit();
+                case 1: task1();
+                    break;
+                case 2: task2();
+                    break;
+                case 3: newSchedule();
+                    break;
+                case 4: showMenu();
+                    break;
+                default: System.out.println("Tokio pasirinkimo nėra!");
+                    break;
+            }
+            System.out.print("\nĮveskite meniu pasirinkimą: \n");
+            choice = input.nextInt();
+            input.nextLine();
+        }
+    }
+
+    //Simple menu of program functions
+
+    private void showMenu(){
+        System.out.println("\n**********************************************************************");
+        System.out.println("*                     Paskolos grąžinimo grafikas                    *");
+        System.out.println("**********************************************************************");
+        System.out.println("*[1] 1-os užduoties failo paskolos grąžinimo grafiko generavimas     *");
+        System.out.println("*[2] 2-os užduoties failo paskolos grąžinimo grafiko generavimas     *");
+        System.out.println("*[3] Naujo grafiko generavimas                                       *");
+        System.out.println("*[4] `Pakartoti meniu pasirinkimus                                   *");
+        System.out.println("*[0] `Išeiti iš sistemos                                             *");
+        System.out.println("**********************************************************************\n");
+    }
+
+    //First task: date = 2017-04-15, loanSize = 5000, numberOfIncomes = 24, interestRate = 12.
+    //Date format 04/15/2017
+
+    private void task1(){
+        try {
+            //Writing csv file with initialized parameters for ammortization schedule.
+            FileWriter fileWriter = new FileWriter("task1.csv");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("\"Payment #\";\"Payment date\";\"Remaining amount\";\"principal payment\";\"Interest payment\";\"Total payment\";\"Interest rate\"");
+            Date date = new SimpleDateFormat("MM/dd/yyyy").parse("04/15/2017");
+            BigDecimal loanSize = new BigDecimal("5000");
+            AmortizationSchedule a = new AmortizationSchedule(date,12,loanSize,24,0);
+            a.generateSchedule( printWriter);
+            printWriter.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //Second task: date = 2017-04-15, loanSize = 5000, numberOfIncomes = 24, interestRate = 12 till 2017-09-01.
+    //On 2017-09-02 interestRate = 9.
+    private void task2(){
+        try {
+            //Writing to csv file.
+            FileWriter fileWriter = new FileWriter("task2.csv");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("\"Payment #\";\"Payment date\";\"Remaining amount\";\"Principal payment\";\"Interest payment\";\"Total payment\";\"Interest rate\"");
+
+            //Initializing schedule parameters.
+            Date date = new SimpleDateFormat("MM/dd/yyyy").parse("04/15/2017");
+            BigDecimal loanSize = new BigDecimal("5000");
+            AmortizationSchedule a = new AmortizationSchedule(date,12,loanSize,24,5);
+
+            //Generating schedule including 5 months of payment.
+            a.generateSchedule( printWriter);
+
+            //Changing schedule parameters and generating remaining payments.
+            a.setInterestRate(9);
+            a.setMonths(0);
+            a.setNumberOfIncomes(24-5);
+
+            //Generating schedule.
+            a.generateSchedule(printWriter);
+            printWriter.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Generating new schedule with user parameters.
+    private void newSchedule(){
+        try {
+            AmortizationSchedule a = new AmortizationSchedule();
+
+            //Setting date
+            System.out.println("\nĮveskite skolos pirmos įmokos datą formatu MM/DD/YYYY:");
+            String dateLine = input.nextLine();
+            Date date = new SimpleDateFormat("MM/dd/yyyy").parse(dateLine);
+            a.setDate(date);
+
+            //Setting number of incomes.
+            System.out.println("\nĮveskite įmokų skaičių:");
+            a.setNumberOfIncomes(input.nextInt());
+            input.nextLine();
+
+            //Setting loan size.
+            System.out.println("\nĮveskite paskolos dydį:");
+            BigDecimal loanSize = new BigDecimal(input.nextDouble());
+            a.setLoanSize(loanSize);
+            input.nextLine();
+
+            //Setting interest rate.
+            System.out.println("\nĮveskite metines palūkanas:");
+            a.setInterestRate(input.nextDouble());
+            input.nextLine();
+
+            //Generating new csv file with ammortization schedule.
+            FileWriter fileWriter = new FileWriter("newSchedule.csv");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            printWriter.println("\"Payment #\";\"Payment date\";\"Remaining amount\";\"Principal payment\";\"Interest payment\";\"Total payment\";\"Interest rate\"");
+            a.generateSchedule(printWriter);
+            printWriter.close();
+        } catch (ParseException e) {
+            System.out.println("Neteisingai įvesta data!");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e){
+            System.out.println("Neteisinga įvestis!");
+            input.next();
+        }
+    }
+
+    //Exiting program.
+    private void exit(){
+        System.exit(1);
+    }
+
+}
